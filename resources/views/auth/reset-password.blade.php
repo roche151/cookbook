842eec8bf55f25f73ed23bd7cccc8b105664ae31
roche151 <73205366+roche151@users.noreply.github.com>
2025-12-28 17:13:57 +0000

    feat: Update email input field to be readonly and enhance styling

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -13,9 +13,9 @@
 
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
-            <input id="email" type="email" name="email" autocomplete="username"
-                   value="{{ old('email', $request->email) }}"
-                   class="form-control @error('email') is-invalid @enderror" required autofocus>
+                     <input id="email" type="email" name="email" autocomplete="username"
+                         value="{{ old('email', $request->email) }}" readonly
+                         class="form-control bg-body-secondary text-muted @error('email') is-invalid @enderror" required autofocus>
             @error('email')
                 <div class="invalid-feedback">{{ $message }}</div>
             @enderror

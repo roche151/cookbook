2d32e099627cdb28950ef003459abc1b19ddfb5f
roche151 <73205366+roche151@users.noreply.github.com>
2025-12-31 10:46:21 +0000

    fix: Update recipe grid layout to use 'row-cols-xl-3' for better responsiveness

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -139,7 +139,7 @@
         </div>
 
         @if($recipes->count())
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+            <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-4">
                 @foreach($recipes as $recipe)
                     <div class="col">
                         @include('recipes._card', ['recipe' => $recipe])

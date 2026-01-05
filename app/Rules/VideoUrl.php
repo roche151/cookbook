c6083e0af357067eb62571313100dd1d21201a16
roche151 <73205366+roche151@users.noreply.github.com>
2026-01-05 14:22:56 +0000

    refactor: Restrict video URL validation to only YouTube links

--- app/Rules/VideoUrl.php
+++ app/Rules/VideoUrl.php
@@ -10,20 +10,8 @@ class VideoUrl implements Rule
     {
         if (empty($value)) return true;
         $value = trim($value);
-        // YouTube
+        // Only YouTube
         if (preg_match('#^(https?://)?(www\.)?(youtube\.com/watch\?v=|youtu\.be/)[\w-]{11}#i', $value)) {
-            return true;
-        }
-        // Instagram (reels or posts)
-        if (preg_match('#^(https?://)?(www\.)?instagram\.com/(reel|p)/[\w-]+#i', $value)) {
-            return true;
-        }
-        // TikTok
-        if (preg_match('#^(https?://)?(www\.)?tiktok\.com/@[\w.-]+/video/\d+#i', $value)) {
-            return true;
-        }
-        // Facebook (video URLs)
-        if (preg_match('#^(https?://)?(www\.)?facebook\.com/.+/videos/(\d+)#i', $value)) {
             return true;
         }
         return false;
@@ -31,6 +19,6 @@ class VideoUrl implements Rule
 
     public function message()
     {
-        return 'The :attribute must be a valid YouTube, Instagram, TikTok, or Facebook video URL.';
+        return 'The :attribute must be a valid YouTube video URL.';
     }
 }
